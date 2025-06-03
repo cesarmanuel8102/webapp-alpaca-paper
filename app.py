50b55609ed4afc5e387fee83cf2bced2ff2f2ede
cesarmanuel8102 <cesarmanuel8102@gmail.com>
2025-06-03 17:15:43 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "WebApp Alpaca PaperTrading Activa"
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Render necesita que se use este puerto
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
