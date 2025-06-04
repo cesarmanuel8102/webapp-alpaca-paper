683ab86cc9fc475456362e11ef244164a66f94a4
cesarmanuel8102 <cesarmanuel8102@gmail.com>
2025-06-03 22:27:30 -0400

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def index():
-    return "WebApp con SDK de Tastytrade funcionando correctamente."
-if __name__ == "__main__":
-    app.run(debug=True)
